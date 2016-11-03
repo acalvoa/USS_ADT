5ee6b97d063a90ed87c6ee75f7c6908a4e83b93c
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-11-03 12:35:26 -0300

    se actualiza

--- src/client/app/modules/dashboard/lugares/lugares.component.ts
+++ src/client/app/modules/dashboard/lugares/lugares.component.ts
@@ -14,93 +14,83 @@ export class LugaresComponent {
 	private rest:RestService;
 	private view:string;
 	private lugares:any[];
+	private sedes:any[];
 	private dataEdit:any;
 	private dataIn:any;
-	private sedes:any[];
 	//CONSTRUCTOR
 	constructor(rest:RestService){
-		this.rest = rest;
 		this.view = 'visor';
+		this.rest = rest;	
 		this.fetch();
-		this.resetForm();
 		this.fetchSedes();
+		this.resetForm();
 	}
-	//TRAE DATA
 	fetch(){
 		this.rest.get('/lugar').subscribe(
-			data => {
-				this.lugares = data;
-			},
-			err => console.error(err)
+		    data => {
+		    	this.lugares = data;
+		    },
+		    err => console.error(err)
 		);
 	}
-
 	fetchSedes(){
 		this.rest.get('/sedes').subscribe(
-			data => {
-				if(data.response == 200){
-					this.sedes = data.sedes; 
-				}
-			},
-			err => console.error(err)
+		    data => {
+		    	if(data.response == 200){
+		    		this.sedes = data.sedes;
+		    	}
+		    },
+		    err => console.error(err)
 		);
 	}
-
 	save(event:any){
 		event.preventDefault();
-		
 		if(this.view == 'form'){
-		this.rest.post(this.dataIn,'/lugar').subscribe(
-			data => {
-				data.SEDE = this.searchSede(data.SEDE);
-				this.lugares.push(data);
-				this.view = 'visor';
-				this.resetForm();
-			},
-			err => console.error(err)
-		);
-	}
-	else if(this.view == 'edit'){
-				this.rest.put(this.dataEdit.ID_LUGAR,'/lugar',this.dataIn).subscribe(
-			data => {
-				this.lugares[this.lugares.indexOf(this.dataEdit)] = data;
-				this.view = 'visor';
-				this.resetForm();
-			},
-			err => console.error(err)
-		);
-			}
-		
+			this.rest.post(this.dataIn, '/lugar').subscribe(
+			    data => {
+			    	data.SEDE = this.searchSede(data.SEDE);
+			    	this.lugares.push(data);
+			    	this.view = 'visor';
+			    	this.resetForm();
+			    },
+			    err => console.error(err)
+			);
+		}
+		else if(this.view == 'edit'){
+			this.rest.put(this.dataEdit.ID_LUGAR, '/lugar', this.dataIn).subscribe(
+			    data => {
+			    	this.lugares[this.lugares.indexOf(this.dataEdit)] = data;
+			    	this.view = 'visor';
+			    	this.resetForm();
+			    },
+			    err => console.error(err)
+			);
+		}
 	}
 	searchSede(value:number){
-		for(let i=0; i<this.sedes.length; i++)
-			{ if(this.sedes[i].ID_SEDE == value)
-				return this.sedes[i];
-			}
+		for(let i=0; i<this.sedes.length; i++){
+			if(this.sedes[i].ID_SEDE == value) return this.sedes[i];
+		}
 		return null;
 	}
-	goEdit(obj:any){	
+	goEdit(obj:any){
 		this.dataEdit = obj;
 		this.dataIn.NOMBRE_LUGAR = obj.NOMBRE_LUGAR;
 		this.dataIn.SEDE = obj.SEDE.ID_SEDE;
 		this.view = 'edit';
-
 	}
-
 	delete(obj:any){
-
 		this.rest.delete(obj.ID_LUGAR, '/lugar').subscribe(
-			data => {
-				this.lugares.splice(this.lugares.indexOf(obj),1);
-			},
-			err => console.error(err)
+		    data => {
+		    	this.lugares.splice(this.lugares.indexOf(obj),1);
+		    },
+		    err => console.error(err)
 		);
-
 	}
 	resetForm(){
 		this.dataIn = {
-		NOMBRE_LUGAR: '',
-		SEDE: ''
+			NOMBRE_LUGAR: '',
+			SEDE: ''
 		};
 	}
 }
