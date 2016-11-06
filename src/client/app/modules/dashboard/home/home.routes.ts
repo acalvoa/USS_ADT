87050f98c9ba1741f5d70a59da30c183a680c2c2
Angelo Calvo A <angelo.calvoa@gmail.com>
2016-11-06 16:01:55 -0300

    MAS Y MAS FUNCIONES

---
+++ src/client/app/modules/dashboard/home/home.routes.ts
@@ -0,0 +1,14 @@
+import { Route } from '@angular/router';
+import { HomeComponent } from './index';
+
+export const HomeRoutes: Route[] = [
+  	{
+    	path: '',
+    	redirectTo: 'dashboard',
+    	pathMatch: 'full'
+  	},
+  	{
+    	path: 'dashboard',
+    	component: HomeComponent
+  	}
+];
