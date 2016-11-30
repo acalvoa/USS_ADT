1f50421d15e5c648411474773e05f9f9db1185e0
Santiago Vergara <santiago.solar@hotmail.com>
2016-11-30 20:01:34 -0300

    Inicio Dashboard
    Probando saludo al usuario

--- src/client/app/modules/dashboard/dashboard.component.ts
+++ src/client/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/index';
+import { Router } from '@angular/router';
+
 /**
 *	This class represents the lazy loaded DashboardComponent.
 */
@@ -12,8 +15,15 @@ import { Component } from '@angular/core';
 
 export class DashboardComponent {
 	public sidebar:boolean;
-	constructor() {
+	private user:any;
+	private user_service:UserService;
+	private router:Router;
+
+	constructor(user:UserService, router:Router) {
 		this.sidebar = true;
+		this.router = router;
+		this.user_service = user;
+		this.user = this.user_service.getUser();
 	}
 	toggleSidebar() {
 		this.sidebar = !this.sidebar;
